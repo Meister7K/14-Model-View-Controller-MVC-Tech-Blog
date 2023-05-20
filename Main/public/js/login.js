9eb95299a61bcd937dce757786f7a2f8bde43da9
7K <kmanfinkel17@gmail.com>
2023-05-20 18:23:15 -0500

    set up login route in public js files and logo

--- Main/public/js/login.js
+++ Main/public/js/login.js
@@ -1,1 +1,48 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    // Collect values from the login form
+    const email = document.getElementById('emailLogInput').value.trim();
+    const password = document.getElementById('passwordLogInput').value.trim();
+  
+    if (email && password) {
+      // Send a POST request to the API endpoint
+      const response = await fetch('/api/member/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        // If successful, redirect the browser to the profile page
+        document.location.replace('/profile');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  };
 
+  const signupFormHandler = async (event) =>{
+    event.preventDefault();
+
+    const username = document.getElementById('usernameSignInput').value.trim();
+    const email = document.getElementById('emailSignInput').value.trim();
+    const password = document.getElementById('passwordSignInput').value.trim();
+
+    if(username && email && password){
+        const response = await fetch('/api/member', {
+            method: 'POST',
+            body: JSON.stringify({username, email, password}),
+            headers: {'Content-type':'application/json'},
+        });
+        if(response.ok){
+            document.location.replace('/profile');
+        } else {
+            alert(response.statusText);
+        }
+    }
+  };
+
+  document.getElementById('login-form').addEventListener('submit', loginFormHandler);
+  
+  document.getElementById('signup-form').addEventListener('submit', signupFormHandler);
--- Main/server.js
+++ Main/server.js
@@ -6,6 +6,7 @@ const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helpers');
+const { json } = require('sequelize');
 const hbs = exphbs.create({ helpers });
 
 const app = express();
@@ -26,12 +27,14 @@ const sesh = {
   })
 };
 
-app.use(routes);
-app.use(session(sesh));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 //app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sesh));
+app.use(routes);
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -39,3 +42,9 @@ app.set('view engine', 'handlebars');
 sequelize.sync({ force: false}).then(()=>{
   app.listen(PORT, ()=> console.log(`Listening on PORT ${PORT}`))
 });
+
+
+//!server is order sensitive
+// json
+// url encoded 
+// seshion can then see the json data 
